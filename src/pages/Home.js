aa66b04916569ecb11734e7551aa0c57ac9c5b4f
RYANRANAUT <ryanranaut@gmail.com>
2021-07-15 19:00:08 +0530

    created imput and fetch results from tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is Home page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = event => {
+    if (event.keyCode === 13) handleClick();
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={handleChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={handleClick}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
