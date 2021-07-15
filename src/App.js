c9651fdcfb2750e28989f767fc289cbaa8785c36
RYANRANAUT <ryanranaut@gmail.com>
2021-07-15 13:06:01 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is Home Page
+      </Route>
+
+      <Route exact path="/Ryan">
+        This is Ryans page
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
