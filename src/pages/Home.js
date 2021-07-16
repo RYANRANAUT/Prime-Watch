3382255aa77c00aeda78b06c0d9d6ead68ddd91c
RYANRANAUT <ryanranaut@gmail.com>
2021-07-16 18:31:55 +0530

    displayed show results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,41 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const handleChange = event => {
     setInput(event.target.value);
   };
 
   const handleClick = () => {
-    // https://api.tvmaze.com/search/shows?q=men
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = event => {
     if (event.keyCode === 13) handleClick();
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>Sorry No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -33,6 +49,7 @@ const Home = () => {
       <button type="button" onClick={handleClick}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
